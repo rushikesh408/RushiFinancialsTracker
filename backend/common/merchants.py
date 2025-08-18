1c10acafd617055f7d7f9d51f17f53cd8e17ebf3
rushikesh408 <rushikeshveeramally@gmail.com>
2025-08-17 18:46:27 -0700

    added get-api functionality

--- backend/common/merchants.py
+++ backend/common/merchants.py
@@ -5,19 +5,30 @@ from bson.json_util import dumps
 
 import json
 
+total_exp = {
+    
+}
+
+
 def list_merchants():
+    expenses_list = []
     mongo_db.get_database()
     print("Fetching merchants from the database")
     merchant_collection =  mongo_db.get_collection().find()
+
     list_merchants = list(merchant_collection)
+    print(f'length of list= {len(list_merchants)}')
     json_merchants = dumps(list_merchants)
-    
-    for json_merchant in json_merchants:
-        print(json_merchant)
-        #merchantsjsonoutput = json.loads(json_merchant).get("json_output")
-        #print(json.loads(merchantsjsonoutput).get("merchant_name")) 
- 
-        
-            
-    return True
+
+    bill = json.loads(json_merchants)
+    print(f'length of bill= {len(bill)}')
+
+    for r in range(0, len(bill)):
+        bill = json.loads(json_merchants)[r].get('json_output')
+        merchant_name = json.loads(bill).get('merchant_name')
+        total_exp[merchant_name] =  total_exp.get(merchant_name,0.0)+  json.loads(bill).get('total_cost')
+
+    return total_exp
+
+   
    
